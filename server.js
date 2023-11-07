76e6dd04eda8ad44644047a04e63bd0254d8f2d5
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-11-07 17:29:20 +0100

    Correzione bug utf-8 esercizio finito con bonus

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
 const dotenv = require("dotenv");
-//avvio 
+
 dotenv.config();
 
 const motivationalPhrase = [
-    "Il successo è/ la somma di piccoli sforzi ripetuti giorno dopo giorno.",
+    "Il successo è la somma di piccoli sforzi ripetuti giorno dopo giorno.",
     "Non importa quanto sia difficile oggi, se continui a spingere, domani sarà migliore.",
     "Le sfide sono l'opportunità di crescere. Non avere paura di esse.",
     "Sii la migliore versione di te stesso ogni giorno.",
-    "Il tuo unico limite è/ la tua mente. Sogna in grande e supera i tuoi limiti.",
-    "Il fallimento è/ solo l'opportunità di iniziare di nuovo, in modo più intelligente.",
-]
-
+    "Il tuo unico limite è la tua mente. Sogna in grande e supera i tuoi limiti.",
+    "Il fallimento è solo l'opportunità di iniziare di nuovo, in modo più intelligente."
+];
 
 function htmlResponse(res, content) {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
     res.end(content);
-};
+}
 
 function randomPhrase(array) {
     const randomIndex = Math.floor(Math.random() * array.length);
@@ -25,12 +24,12 @@ function randomPhrase(array) {
 }
 
 const server = http.createServer(function (req, res) {
-
-    const phrase = randomPhrase(motivationalPhrase)
-    res.end(`<h1>${phrase}</h1>`);
-
-})
+    const phrase = randomPhrase(motivationalPhrase);
+    const htmlContent = `<h1>${phrase}</h1>`;
+    htmlResponse(res, htmlContent);
+    console.log(phrase);
+});
 
 server.listen(port, function () {
-    console.log("server is running on port: " + port)
-})
+    console.log("Server is running on port: " + port);
+});
